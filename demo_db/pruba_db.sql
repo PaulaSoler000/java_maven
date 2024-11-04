af0d4ad9b9caf23e002c1c9256b520e74ae25284
PaulaSoler000 <93978384+PaulaSoler000@users.noreply.github.com>
2024-11-04 20:40:36 +0100

    db dni

--- demo_db/pruba_db.sql
+++ demo_db/pruba_db.sql
@@ -4,8 +4,9 @@ USE prueba_db;
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) NOT NULL
+    correo VARCHAR(100) NOT NULL,
+    dni VARCHAR(100) NOT NULL
 );
 
-INSERT INTO usuarios (nombre, correo) VALUES ('Juan Pérez', 'juan.perez@example.com');
-INSERT INTO usuarios (nombre, correo) VALUES ('María López', 'maria.lopez@example.com');
+INSERT INTO usuarios (nombre, correo, dni) VALUES ('Juan Pérez', 'juan.perez@example.com', '12345678A');
+INSERT INTO usuarios (nombre, correo, dni) VALUES ('María López', 'maria.lopez@example.com', '87654321B');
